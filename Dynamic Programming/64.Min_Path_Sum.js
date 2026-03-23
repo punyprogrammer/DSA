e073454931acf601c19300e8a7e83914d183c437
Amardeep Ganguly <83149058+punyprogrammer@users.noreply.github.com>
2026-03-23 10:19:49 +0530

    Create 64.Min_Path_Sum.js

---
+++ Dynamic Programming/64.Min_Path_Sum.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[][]} grid
+ * @return {number}
+ */
+var minPathSum = function(grid) { 
+    const m = grid.length;
+    const n = grid[0].length;
+    const dp  = Array.from({length:m},()=>Array(n).fill(Infinity));
+    // initialize top row and left column as movement is right and bottom 
+    // these will be min possible cost for this points
+    dp[0][0] = grid[0][0];
+    for(let i = 1 ;i<m;i++) dp[i][0] = grid[i][0]+dp[i-1][0];
+    for(let i = 1 ;i<n;i++) dp[0][i] = grid[0][i]+dp[0][i-1];
+    for(let i = 1;i<m;i++){
+        for(let j = 1;j<n;j++){
+            dp[i][j] = Math.min(dp[i-1][j]+grid[i][j],dp[i][j-1]+grid[i][j])
+        }
+    }
+    return dp[m-1][n-1];
+
+};
