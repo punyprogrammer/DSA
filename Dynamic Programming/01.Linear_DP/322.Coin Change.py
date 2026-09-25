12a920ce94834c63cdf397876b5eef4549e8282d
Amardeep Ganguly <83149058+punyprogrammer@users.noreply.github.com>
2026-09-26 01:19:00 +0530

    Create 322.Coin Change.py

---
+++ Dynamic Programming/01.Linear_DP/322.Coin Change.py
@@ -0,0 +1,11 @@
+class Solution:
+    def coinChange(self, coins: list[int], amount: int) -> int:
+        dp = [float("inf")]*(amount + 1)
+        dp[0]=0
+        for x in range(1,amount+1):
+            for  i in coins:
+                if x >=i:
+                    dp[x] = min(dp[x-i]+1,dp[x])
+        if dp[amount] == float("inf"):
+            return -1
+        return dp[amount]
